9d16797a88296268cc8b7c80b793086f521a9c50
Ivan Guan <ivang12@g.ucla.edu>
2024-02-08 23:47:13 -0800

    implement api for signup page and store in mongodb

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,14 +1,26 @@
 
 from flask import Flask
+from flask_pymongo import PyMongo
 #from config import DevelopmentConfig # Import the configuration class
 from .main.routes import main
 from .api.routes import api
+from .extensions import mongo
+import os
 
 
 def create_app():
     app = Flask(__name__)
    # app.config.from_object(DevelopmentConfig) # Use the config class
+    mongo_uri = os.environ.get('MONGO_URI')
+    if not mongo_uri:
+        raise RuntimeError("MONGO_URI is not set in the environment variables")
+
+    app.config["MONGO_URI"] = mongo_uri
+    mongo.init_app(app)
+
     app.register_blueprint(main)
     app.register_blueprint(api, url_prefix='/api')
 
     return app
+
+
--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash
+from ..extensions import mongo
 import os
 
 api = Blueprint('api', __name__)
@@ -28,3 +30,26 @@ def upload_file():
         filename = secure_filename(file.filename)
         file.save(os.path.join(UPLOAD_FOLDER, filename))
         return jsonify({'message': 'File successfully uploaded'}), 200
+
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    if not username or not password:
+        return jsonify({"error": "Missing username or password"}), 400
+
+    # Check if user already exists
+    # user = mongo.db.users.find_one({"username": username})
+    # if user:
+    #     return jsonify({"error": "Username already exists"}), 409
+
+    # Hash the password for security
+    hashed_password = generate_password_hash(password)
+
+    # Insert new user into the database
+    print(mongo.db)
+    mongo.db.users.insert_one({"username": username, "password": hashed_password})
+
+    return jsonify({"message": "User created successfully"}), 201
