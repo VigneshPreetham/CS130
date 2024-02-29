38d44a656fe1c29553d3a5f526878e67e27e06ea
Sam Hirschhorn <shirschhorn9@gmail.com>
2024-02-28 21:21:04 -0800

    backend upload_recipe and add_recipe

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -2,7 +2,9 @@ from flask import Blueprint, request, jsonify, current_app
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash
 import os
-import uuid
+import uuid4
+import boto3
+from botocore.exceptions import ClientError
 
 from ..extensions import mongo
 from ..utils.database import MongoDBUserCollection
@@ -21,9 +23,16 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@api.route('/upload', methods=['POST'])
-def upload_file():
-    # Check if the post request has the file part
+@api.route('/uploadrecipe', methods=['POST'])
+def upload_recipe():
+    data = request.get_json()
+    # Check if the post request has the neccesary recipe data
+    if 'name' not in data:
+        return jsonify({'message': 'No name'}), 400
+    if 'recipe' not in data:
+        return jsonify({'message': 'No recipe'}), 400
+    if 'creator_id' not in data:
+        return jsonify({'message': 'No creator'}), 400
     if 'file' not in request.files:
         return jsonify({'message': 'No file part'}), 400
     file = request.files['file']
@@ -31,10 +40,66 @@ def upload_file():
     # empty file without a filename.
     if file.filename == '':
         return jsonify({'message': 'No selected file'}), 400
+    
+    #upload file to s3
     if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(UPLOAD_FOLDER, filename))
-        return jsonify({'message': 'File successfully uploaded'}), 200
+        try:
+            filename = upload_photo_to_s3(file)
+        except ClientError as e:
+            return jsonify({'message': 'Error uploading photo to s3 - ' + e}), 400
+    
+    db = mongo.cx['savor']
+    recipe_collection = db['recipes']
+    name = data.get('name')
+    recipe = data.get('recipe')
+    creator_id = data.get('creator_id')
+    photo_filename = filename
+    recipe_id = str(uuid.uuid4())
+
+    result = recipe_collection.insert_one({"id": recipe_id,
+                                           "name": name,
+                                           "recipe": recipe,
+                                           "photo_filename": photo_filename,
+                                           "creator_id": creator_id})
+    
+    return jsonify({"message": "Recipe successfully created"}), 201
+
+
+def upload_photo_to_s3(file):
+    s3 = boto3.client('s3')
+    filename = secure_filename(file.filename)
+    try:
+        s3.upload_fileobj(file, S3_BUCKET, filename)
+
+    except ClientError as e:
+        print("Error uploading photo:", e)
+        return e
+    
+    # after upload file to s3 bucket, return filename of the uploaded file
+    return filename
+
+@api.route('/addrecipe', methods=['POST'])
+def add_recipe():
+    data = request.get_json()
+    # Check if the post request has the neccesary recipe data
+    if 'user_id' not in data:
+        return jsonify({'message': 'No user_id'}), 400
+    if 'recipe_id' not in data:
+        return jsonify({'message': 'No recipe_id'}), 400
+
+    result = add_recipe_to_user(data.get("user_id"), data.get("recipe_id"))
+
+    return jsonify({"message": "Recipe added to User successfully"}), 200
+
+def add_recipe_to_user(user_id, recipe_id):
+    db = mongo.cx['savor']
+    user_collection = db['users']
+    query = {"user_id": user_id}
+    update = { "$push" : { "recipes" : recipe_id} }
+
+    result = user_collection.update_one(query, update)
+    
+    return result
 
 
 @api.route('/signup', methods=['POST'])
