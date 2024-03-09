9da8bc955c2f17fc5d1949e5c4e77338ccfe2d5f
Ivan Guan <ivang12@g.ucla.edu>
2024-03-09 12:25:33 -0800

    prepare DB for inserting recipes and create a workflow for deploying to fly.io

--- backend/app/utils/database.py
+++ backend/app/utils/database.py
@@ -14,7 +14,6 @@ class MongoDBUserCollection:
         self.mongo = mongo
         self.db = self.mongo.cx['savor']
         self.users_collection = self.db['users']
-        self.food_collection = self.db['food']
     
     def email_exists(self, email):
         user = self.users_collection.find_one({"email": email})
@@ -28,8 +27,9 @@ class MongoDBUserCollection:
         hashed_password = generate_password_hash(password)
         now = datetime.now()
         formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
+        recipe_list = [] 
 
-        self.users_collection.insert_one({"id": user_id, "email": email, "username": username, "password": hashed_password, "created_on": formatted_now})
+        self.users_collection.insert_one({"id": user_id, "email": email, "username": username, "password": hashed_password, "recipes": recipe_list, "created_on": formatted_now})
         user = self.users_collection.find_one({"email": email})
 
         return user
@@ -52,9 +52,26 @@ class MongoDBUserCollection:
             matches = [match for match in matches if match['id'] != exact_match['id']]
             matches.insert(0, exact_match)
 
-        uuids = [match['id'] for match in matches]
-        return uuids
+        return matches
 
+class MongoDBRecipeCollection: 
+    def __init__(self, mongo):
+        self.mongo = mongo
+        self.db = self.mongo.cx['savor']
+        self.food_collection = self.db['food']
+        self.users_collection = self.db['users']
+    
+    def insert_food(self, link_to_s3, creator):
+        now = datetime.now()
+        formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
+        recipe_id = str(uuid.uuid4())
+        users = [] 
+        self.food_collection.insert_one({"id": recipe_id, "link": link_to_s3, "created_by": creator, "users": user, "created_on": formatted_now })
+        recipe = self.food_collection.find_one({"id": recipe_id})
+
+        return recipe
+
+   
 
 class AmazonS3DB:
     def __init__(self):
@@ -64,8 +81,19 @@ class AmazonS3DB:
 
 
     def upload_to_s3(self, file, filename):
-        bucket_name = 'your-bucket-name'
-        s3.upload_fileobj(file, bucket_name, filename)
+        bucket_name = 'savor-recipe'
+        filename_appended = f"{filename}-{int(time.time())}"
+
+        s3.upload_fileobj(
+            file,
+            bucket_name,
+            filename_appended,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+
         file_url = f"https://{bucket_name}.s3.amazonaws.com/{filename}"
         return file_url
 
